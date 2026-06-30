80a82b8cfb0a70d15152d6813734bd45fc0e55ba
Dennis Shlaih <dennisshlaih@gmail.com>
2026-06-29 21:14:38 -0400

    add campus routes (js)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const campusRoutes = require('./routes/campuses');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,6 +11,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'Campuses API running' });
 });
+
+app.use('/campuses', campusRoutes);
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
